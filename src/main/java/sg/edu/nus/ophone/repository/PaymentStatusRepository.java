714010e25eb61817870787717372158d5095dee6
freddie.c.kuo <freddie.c.kuo@gmail.com>
2024-10-03 16:33:51 +0800

    Update Payment Entity
    Combine Payment Status into Payment

--- src/main/java/sg/edu/nus/ophone/repository/PaymentStatusRepository.java
+++ src/main/java/sg/edu/nus/ophone/repository/PaymentStatusRepository.java
@@ -2,8 +2,6 @@ package sg.edu.nus.ophone.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import sg.edu.nus.ophone.model.PaymentStatus;
-
 //code by Team3.Kuo Chi
 public interface PaymentStatusRepository extends JpaRepository <PaymentStatus, Integer> {
 
