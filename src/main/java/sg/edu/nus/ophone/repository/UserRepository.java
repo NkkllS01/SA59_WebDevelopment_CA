8d6bfdb71a7dc4c5e1df01f65633d49bf9a5d446
Cynthia <e1458657@u.nus.edu>
2024-10-03 16:38:49 +0800

    amended from users to user

--- src/main/java/sg/edu/nus/ophone/repository/UserRepository.java
+++ src/main/java/sg/edu/nus/ophone/repository/UserRepository.java
@@ -4,19 +4,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import sg.edu.nus.ophone.model.Users;
+import sg.edu.nus.ophone.model.User;
 
 import java.util.List;
 
 //code by Team3.Cynthia Peh
-public interface UsersRepository extends JpaRepository<Users, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
   @Query("SELECT u FROM Users u WHERE u.id = :id")
-  List<Users> findByUserId(@Param("id") int id);
+  List<User> findByUserId(@Param("id") int id);
 
   @Query("SELECT u FROM Users u WHERE u.email = :email")
-  List<Users> findByUserEmail(@Param("email") String email);
+  List<User> findByUserEmail(@Param("email") String email);
 
   @Query("SELECT u FROM Users u WHERE u.userType =:userType")
-  List<Users> findByUserType(String userType);
+  List<User> findByUserType(String userType);
 }
