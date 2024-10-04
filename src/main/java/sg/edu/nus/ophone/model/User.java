d8b8d2d9f75d3d194ce7dc4ce74ab3062b4c0b0b
Cynthia <e1458657@u.nus.edu>
2024-10-04 13:44:32 +0800

    Amend users
    import persistence.*;

--- src/main/java/sg/edu/nus/ophone/model/User.java
+++ src/main/java/sg/edu/nus/ophone/model/User.java
@@ -2,12 +2,7 @@ package sg.edu.nus.ophone.model;
 
 import java.util.List;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 
 //code by Team3.Cynthia Peh
 @Entity
