53852e1ab15d29d0737dfc75ce8fb4c2078453bd
Jiamin Ng <jiamin.ng@u.nus.edu>
2024-10-05 18:37:40 +0800

    Update OrderRepo, OrderDetailsRepo

--- src/main/java/sg/edu/nus/ophone/repository/OrderDetailsRepository.java
+++ src/main/java/sg/edu/nus/ophone/repository/OrderDetailsRepository.java
@@ -2,11 +2,18 @@ package sg.edu.nus.ophone.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import sg.edu.nus.ophone.model.Order;
 import sg.edu.nus.ophone.model.OrderDetails;
+import sg.edu.nus.ophone.model.OrderDetailsId;
+
+import java.util.List;
 
 //code by Team3.Lian Da
 @Repository
-public interface OrderDetailsRepository extends JpaRepository <OrderDetails, Integer>{
-
+public interface OrderDetailsRepository extends JpaRepository <OrderDetails, OrderDetailsId>{
+    @Query("SELECT od FROM OrderDetails od WHERE od.order = :order")
+    public List<OrderDetails> findByOrder(@Param("order") Order order);
 }
