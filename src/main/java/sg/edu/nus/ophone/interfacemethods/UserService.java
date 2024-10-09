8f8d150867372c821e1ed9f8115409784f1b0a7d
Jiamin Ng <jiamin.ng@u.nus.edu>
2024-10-09 16:16:15 +0800

    Add findUserByUserId in UserService

--- src/main/java/sg/edu/nus/ophone/interfacemethods/UserService.java
+++ src/main/java/sg/edu/nus/ophone/interfacemethods/UserService.java
@@ -1,6 +1,9 @@
 package sg.edu.nus.ophone.interfacemethods;
 
+import sg.edu.nus.ophone.model.User;
+
 public interface UserService {
 
     boolean login(String username,String password);
+    User findUserByUserId(int userId);
 }
