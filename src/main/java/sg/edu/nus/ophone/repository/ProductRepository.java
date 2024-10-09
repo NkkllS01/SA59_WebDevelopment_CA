eab5f76cbfcda3ae71690a91da4dab0829d43b32
Chen Sirui <arthas00@live.com>
2024-10-09 08:01:24 +0800

    Interceptor demo &several queries

--- src/main/java/sg/edu/nus/ophone/repository/ProductRepository.java
+++ src/main/java/sg/edu/nus/ophone/repository/ProductRepository.java
@@ -2,11 +2,19 @@ package sg.edu.nus.ophone.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import sg.edu.nus.ophone.model.Product;
+
+import java.util.List;
 
 //code by Team3.Ng Jiamin
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    @Query("select p from Product p where p.name like %:keyword%")
+    List<Product> findProductsByKeyword(@Param("keyword") String keyword);
 
+    @Query("select p from Product p where p.id = :id")
+    Product findProductById(@Param("id") int id);
 }
