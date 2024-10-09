120b324225968d47b1c14fdd5c7801ca09f0f736
Jiamin Ng <jiamin.ng@u.nus.edu>
2024-10-09 16:17:05 +0800

    Add createReview feature in OrderController

--- src/main/java/sg/edu/nus/ophone/controller/OrderController.java
+++ src/main/java/sg/edu/nus/ophone/controller/OrderController.java
@@ -7,12 +7,18 @@ import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.*;
 import sg.edu.nus.ophone.interfacemethods.OrderInterface;
-import sg.edu.nus.ophone.model.Order;
-import sg.edu.nus.ophone.model.OrderDetails;
-import sg.edu.nus.ophone.model.PaymentRecord;
-import sg.edu.nus.ophone.model.Shipping;
+import sg.edu.nus.ophone.interfacemethods.ProductInterface;
+import sg.edu.nus.ophone.interfacemethods.ReviewInterface;
+import sg.edu.nus.ophone.interfacemethods.UserService;
+import sg.edu.nus.ophone.model.*;
+import sg.edu.nus.ophone.repository.ProductRepository;
 import sg.edu.nus.ophone.service.OrderImplementation;
+import sg.edu.nus.ophone.service.ProductImplementation;
+import sg.edu.nus.ophone.service.ReviewImplementation;
+import sg.edu.nus.ophone.service.UserServiceImp;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
@@ -21,17 +27,37 @@ import java.util.stream.Collectors;
 public class OrderController {
     @Autowired
     private OrderInterface orderService;
-
     @Autowired
     public void setOrderService(OrderImplementation orderImp) {
         this.orderService = orderImp;
     }
 
+    @Autowired
+    private ProductInterface productService;
+    @Autowired
+    public void setProductService(ProductImplementation productImp) {
+        this.productService = productImp;
+    }
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    public void setUserService(UserServiceImp userImp) {
+        this.userService = userImp;
+    }
+
+    @Autowired
+    private ReviewInterface reviewService;
+    @Autowired
+    public void setReviewService(ReviewImplementation reviewImp) {
+        this.reviewService = reviewImp;
+    }
+
     @GetMapping ("/orders")
     // Http session data to store userId as an attribute
     public String displayOrders(Model model, HttpSession session, Locale locale) {
-//        int username = (int) session.getAttribute("username");
-//        List<Order> orders = orderService.findByUserId(username);
+//        int userId = (int) session.getAttribute("userId");
+//        List<Order> orders = orderService.findByUserId(userId);
         List<Order> orders = orderService.findByUserId(1);
         List<Order> orderList = orders.stream()
                 .filter(order -> !order.getOrderStatus().equalsIgnoreCase("In cart"))
@@ -95,10 +121,46 @@ public class OrderController {
          }
      }
 
-     @GetMapping("/product/{id}/review")
-    public String reviewProduct(@PathVariable("id") Long productId, Model model, HttpSession session) {
-        return "product-review";
-     }
+    @GetMapping("/product/{id}/review")
+    public String reviewProduct(@PathVariable("id") Long productId,
+                                @RequestParam("orderId") long orderId,
+                                Model model, HttpSession session) {
+        Product product = productService.getProductById(productId);
+        Order order = orderService.findByOrderId(orderId);
+        Shipping shipping = order.getShipping();
+        String shippingStatus = shipping.getShippingStatus();
+
+        if (shippingStatus.equalsIgnoreCase("Delivered")) {
+            model.addAttribute("order", order);
+            model.addAttribute("product", product);
+            model.addAttribute("review", new Review());
+            return "product-review";
+        } else {
+            return "product-review-fail";
+        }
+    }
+
+    @PostMapping("/product/{id}/review/create")
+    public String createProductReview(@PathVariable("id") Long productId,
+                                      @RequestParam("orderId") Long orderId,
+                                      @ModelAttribute Review review,
+                                      Model model, HttpSession session) {
+//        int userId = (int) session.getAttribute("userId");
+//        User user = userService.findUserByUserId(userId);
+        User user = userService.findUserByUserId(1);
+        Product product = productService.getProductById(productId);
+
+        Order order = orderService.findByOrderId(orderId);
+        Shipping shipping = order.getShipping();
+        String shippingStatus = shipping.getShippingStatus();
+
+        review.setProduct(product);
+        review.setUser(user);
+        review.setDate(LocalDate.now());
+
+        reviewService.createNewReview(review);
+        return "product-review-success";
+    }
 
 }
 
