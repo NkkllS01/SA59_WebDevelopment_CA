711b69f354b83b3a8a9134116dce4dd26f83ed4b
Jiamin Ng <jiamin.ng@u.nus.edu>
2024-10-17 16:30:40 +0800

    Staff can select brand from dropdown when updating/creating product
    Change brand to display NAME instead of ID. User can select brand from drop-down list instead of inputting the brand id.

---
+++ src/main/java/sg/edu/nus/ophone/interfacemethods/BrandInterface.java
@@ -0,0 +1,9 @@
+package sg.edu.nus.ophone.interfacemethods;
+
+import sg.edu.nus.ophone.model.Brand;
+
+import java.util.List;
+
+public interface BrandInterface {
+    public List<Brand> findAllBrands();
+}
